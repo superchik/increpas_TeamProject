5d54fa064b6c9f181cc82bcbcd526b2307c1d633
nmnmmnmn <nmnmmnmn@DESKTOP-GFP78RD>
2021-11-17 18:12:45 +0900

    review

--- src/main/java/com/ott/app/ReviewController.java
+++ src/main/java/com/ott/app/ReviewController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ReviewController {
 
+	
 	@RequestMapping("/review")
 	public String review() {
 		return "review";
