c9373d8e1eff278fec3dfbab371e10989feddb05
kmjkm <kmjkm@192.168.0.12>
2021-12-09 13:33:19 +0900

    2021-12-09 import 정리

--- src/main/java/com/ott/app/UserController.java
+++ src/main/java/com/ott/app/UserController.java
@@ -1,24 +1,16 @@
 package com.ott.app;
 
-import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.ott.Util.FileRenameUtil;
 import com.ott.dao.UserDAO;
-import com.ott.user.vo.ImgVO;
 import com.ott.user.vo.UserVO;
 @Controller
 public class UserController {
